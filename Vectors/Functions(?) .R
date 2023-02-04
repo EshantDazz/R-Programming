7283148c5d2e3b5df73440169b26034a88b026b0
Eshant Das <95016576+EshantDazz@users.noreply.github.com>
2023-02-04 11:11:01 +0530

    Add files via upload

---
+++ Vectors/Functions(?) .R
@@ -0,0 +1,24 @@
+#Functions in R
+
+#seq()
+#rep()
+#c()
+#is.numeric(), is.integer(), is.double(), is.character()
+
+#typeof()
+
+#sqrt()
+#paste()
+
+
+#WE CAN USE ?  BEFORE THE FUNCTION
+?rnorm()
+?sqrt()
+?c()
+
+seq(from=10,to=20,length.out=10). #like linspace in numpy
+rep(5:6,times=5)
+rep(5:6,each=5)
+
+a<-c(4L,16L)
+sqrt(a)
