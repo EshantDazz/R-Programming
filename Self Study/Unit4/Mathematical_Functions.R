d4b1d9cb0da86fa95e4571ce4b258bd1a2377fbe
Eshant Das <95016576+EshantDazz@users.noreply.github.com>
2023-04-21 20:39:33 +0530

    Add files via upload

---
+++ Self Study/Unit4/Mathematical_Functions.R
@@ -0,0 +1,31 @@
+x<-4L
+y=-10L
+
+
+abs(y)
+sqrt(x)
+exp(x)
+log(x)
+log10(x)
+sin(x)
+cos(x)
+tan(x)
+asin(x)           #reverse sin
+acos(x)
+
+
+z<-56.34
+floor(z)
+log2(z)
+ceiling(z)
+round(z)
+#sign(x) - returns the sign of x (-1 for negative, 0 for zero, 1 for positive)
+sign(y)
+sign(z)
+
+
+v<-c(34,54,15)
+min(v)
+max(v)
+sum(v)
+prod(v)
