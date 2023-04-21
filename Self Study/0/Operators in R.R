52e59f202ba5e6216be1ff66c828b3e3629f024c
Eshant Das <95016576+EshantDazz@users.noreply.github.com>
2023-04-21 22:20:43 +0530

    Add files via upload

---
+++ Self Study/0/Operators in R.R
@@ -0,0 +1,22 @@
+Arithmetic Operators:
+  These operators are used to perform arithmetic operations between two values, and include +, -, *, /, ^ (exponentiation), and %% (modulus).
+
+Relational Operators:
+  These operators are used to compare two values, and return a logical value of TRUE or FALSE. They include == (equal to), != (not equal to), > (greater than), < (less than), >= (greater than or equal to), and <= (less than or equal to).
+
+Logical Operators:
+  These operators are used to combine or negate logical values, and include & (and), | (or), and ! (not).
+
+Assignment Operators:
+  These operators are used to assign values to variables, and include = and <-.
+
+Special Operators:
+  These operators are used for specific purposes, and include %*% (matrix multiplication), %/% (integer division), : (sequence generation), and ? (help documentation).
+
+Bitwise Operators:
+  These operators are used to perform bitwise operations between two integers, and include & (bitwise and), | (bitwise or), ~ (bitwise not), and ^ (bitwise xor).
+
+Miscellaneous Operators:
+  These operators do not fit into any of the above categories, and include $ (access a specific element in a list), [[ (access a specific element in a list or data frame), () (call a function), and : (colon operator, used for creating sequences).
+                                                                                                                       
+                                                                                                                       It's worth noting that some of these operators can be overloaded, meaning that their behavior may differ depending on the types of the operands they are applied to. For example, the + operator can be used for addition, concatenation of strings, and union of sets.
